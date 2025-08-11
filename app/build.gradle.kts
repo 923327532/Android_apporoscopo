34626345b6a466d6b39f08b06d0e71345310f4c9
Roberto Carlos Lopez <roberto.lopez@tecsup.edu.pe>
2025-08-10 23:18:46 -0500

    Tu mensaje del commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -26,30 +28,43 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
-    buildFeatures{
-        viewBinding=true
+
+    buildFeatures {
+        viewBinding = true
     }
-}
 
-dependencies {
-    //nav components
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    kotlin {
+        jvmToolchain(21)
+    }
 
-    //
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.activity:activity-ktx:1.7.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    dependencies {
+        // Navigation components
+        implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+        implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+        // Dagger Hilt
+        implementation("com.google.dagger:hilt-android:2.48")
+        kapt("com.google.dagger:hilt-compiler:2.48")
+
+        // AndroidX & UI
+        implementation("androidx.core:core-ktx:1.9.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("com.google.android.material:material:1.9.0")
+        implementation("androidx.activity:activity-ktx:1.7.2")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+        // Testing
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    }
 }
